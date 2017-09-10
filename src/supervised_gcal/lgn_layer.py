7bc662384db1227cf925ea1fcac91047fb537625
hernanbari <hernanfb@live.com.ar>
2017-09-10 15:48:01 -0300

    Kickoff sparse_layers

--- src/supervised_gcal/lgn_layer.py
+++ src/supervised_gcal/lgn_layer.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from src.supervised_gcal.layer import Layer
 from src.supervised_gcal.utils import custom_sigmoid, get_gaussian, circular_mask, normalize
@@ -15,8 +16,10 @@ class LissomLGNLayer(Layer):
         super().__init__(input_shape, self_shape)
 
     def _get_weight_variable(self, shape, sigma, radius=20):
-        return torch.nn.Parameter(torch.Tensor(normalize(circular_mask(get_gaussian(shape, sigma),
-                                                                       radius=radius))))
+        array = normalize(circular_mask(get_gaussian(shape, sigma), radius=radius))
+        nnz_idx = np.nonzero(array)
+        ans = torch.nn.Parameter(torch.sparse.FloatTensor(torch.from_numpy(np.asarray(nnz_idx)), torch.from_numpy(array[nnz_idx]), torch.Size(array.shape)))
+        return ans
 
     def _setup_variables(self):
         sigma_center_weights_matrix = self._get_weight_variable(self.afferent_weights_shape, self.sigma_center)
@@ -27,6 +30,11 @@ class LissomLGNLayer(Layer):
             self.weights = torch.nn.Parameter((sigma_sorround_weights_matrix - sigma_center_weights_matrix).data)
 
     def forward(self, input):
+        # TODO: pytorch implements sparse matmul only sparse x dense -> sparse and sparse x dense -> dense
         processed_input = self.process_input(input)
-        var = custom_sigmoid(self.min_theta, self.max_theta, self.lgn_factor * torch.matmul(processed_input, self.weights.data))
+        import ipdb; ipdb.set_trace()
+        torch.spmm()
+        var = torch.matmul(processed_input, self.weights.data)
+        var = custom_sigmoid(self.min_theta, self.max_theta,
+                             self.lgn_factor * var)
         return var
