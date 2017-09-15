f06771fe58f9a7a1536c36aca9a2c8722dccf837
rmatuk <rosana.matuk@gmail.com>
2017-09-15 20:48:58 -0300

    Rosana first commit with tests, new comments, and euclidean distance function.

--- src/supervised_gcal/utils/math.py
+++ src/supervised_gcal/utils/math.py
@@ -1,11 +1,22 @@
-import numpy as np
+"""
+This module contains auxiliary math functions.
+"""
 
+import numpy as np
 import torch
 
 
 def euclidian_distances(x, y, mu_x, mu_y):
+    """
+    This function implements the euclidean distance between two 2-dimensional vectors.
+    """
     return np.sqrt(((x - mu_x) ** 2 + (y - mu_y) ** 2))
 
+def euclidean_distance_general(x, y):
+    """
+    This function implements the euclidean distance between two n-dimensional vectors as numpy arrays.
+    """
+    return np.sqrt(np.sum((x - y) ** 2))
 
 def gaussian(x, y, mu_x, mu_y, sigma):
     num = np.power(x - mu_x, 2) + np.power(y - mu_y, 2)
