c5afb8a8b38586340c86ac02b9e831e8443c1afa
hernanbari <hernanfb@live.com.ar>
2017-09-17 23:21:19 -0300

    Refactors dense_weights_to_sparse

--- src/supervised_gcal/utils/weights.py
+++ src/supervised_gcal/utils/weights.py
@@ -44,19 +44,12 @@ def apply_circular_mask_to_weights(matrix, radius):
 
 
 def dense_weights_to_sparse(matrix):
-    indexes_columns = []
-    nnz_values = []
-    for weights in matrix:
-        nnz_idx = np.nonzero(weights)[0]
-        indexes_columns.append(nnz_idx)
-        nnz_values.append(weights[nnz_idx])
-    flatten_nnz_values = np.asarray(nnz_values).flatten()
-
-    flatten_idx_cols = np.asarray(indexes_columns).flatten()
-
-    indexes_rows = [[idx_r] * len(r[0]) for idx_r, r in enumerate(indexes_columns)]
-    flatten_idx_rows = np.asarray(indexes_rows).flatten()
-
-    tuple_flatten_indexes = np.array([flatten_idx_rows, flatten_idx_cols])
-    return torch.sparse.FloatTensor(torch.from_numpy(tuple_flatten_indexes), torch.from_numpy(flatten_nnz_values),
+    """
+    Transforms a torch dense tensor to sparse
+    http://pytorch.org/docs/master/sparse.html
+    """
+    nnz_mask = matrix != 0
+    nnz_values = matrix[nnz_mask]
+    nnz_indexes = nnz_mask.nonzero()
+    return torch.sparse.FloatTensor(nnz_indexes.t(), nnz_values,
                                     torch.Size([int(matrix.shape[0]), int(matrix.shape[1])]))
