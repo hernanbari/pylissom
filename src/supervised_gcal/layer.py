709ef54776112b9b49efdb3e6496d4a52e0fc529
hernanbari <hernanfb@live.com.ar>
2017-10-22 18:38:47 -0300

    Moves forward and matmul to Layer

--- src/supervised_gcal/layer.py
+++ src/supervised_gcal/layer.py
@@ -1,20 +1,23 @@
 import numpy as np
-
 import torch
 
 
 class Layer(torch.nn.Module):
-    def __init__(self, input_shape, self_shape, min_theta, max_theta, name=''):
-        super().__init__()
+    def __init__(self, input_shape, self_shape, min_theta, max_theta, strength, sparse=False, name=''):
+        super(Layer).__init__()
+        self.sparse = sparse
+        self.strength = strength
         self.max_theta = max_theta
         self.min_theta = min_theta
         self.name = name
         self.self_shape = self_shape
         self.input_shape = input_shape
         self.activation_shape = torch.Size((1, int(np.prod(self.self_shape))))
-        self.weights = []
+        self.weights = None
         self.epoch = -1
         self.batch_idx = 0
+        self.input = None
+        self.activation = None
         self._setup_weights()
 
     def _setup_weights(self):
@@ -35,3 +38,17 @@ class Layer(torch.nn.Module):
         self.batch_idx = 0
         super(self).train(mode=mode)
 
+    def forward(self, stimulus):
+        self.input = stimulus
+        weighted_sum = self.strength * self.matmul(stimulus.data, self.weights.data)
+        activation = self.piecewise_sigmoid(self.min_theta, self.max_theta, weighted_sum)
+        self.activation = torch.autograd.Variable(activation)
+        return self.activation
+
+    def matmul(self, vector, matrix):
+        if not self.sparse:
+            return torch.matmul(vector, matrix)
+        else:
+            # Pytorch implements sparse matmul only sparse x dense -> sparse and sparse x dense -> dense,
+            # That's why it's reversed
+            return torch.matmul(matrix.t(), vector.t()).t()
--- src/supervised_gcal/lgn_layer.py
+++ src/supervised_gcal/lgn_layer.py
@@ -1,47 +1,29 @@
-import torch
 from src.supervised_gcal.layer import Layer
 from src.supervised_gcal.utils.weights import get_gaussian_weights_variable
 
 
 class LGNLayer(Layer):
-    def __init__(self, input_shape, self_shape, on, sigma_center=0.1, sigma_sorround=10, min_theta=0.0, max_theta=1.0,
-                 lgn_factor=1.0, radius=10, sparse=False, name=''):
-        self.sparse = sparse
+    def __init__(self, input_shape, self_shape, on, sigma_center=0.1, sigma_surround=10, min_theta=0.0, max_theta=1.0,
+                 strength=1.0, radius=10, sparse=False, name=''):
         self.radius = radius
-        self.lgn_factor = lgn_factor
         self.on = on
-        self.sigma_sorround = sigma_sorround
+        self.sigma_surround = sigma_surround
         self.sigma_center = sigma_center
         name = ('on' if on else 'off') if name == '' else name
-        super(LGNLayer).__init__(input_shape, self_shape, min_theta, max_theta, name)
+        super(LGNLayer).__init__(input_shape, self_shape, min_theta, max_theta, strength, sparse, name)
 
     def _setup_weights(self):
         sigma_center_weights_matrix = get_gaussian_weights_variable(self.input_shape, self.self_shape,
                                                                     self.sigma_center,
                                                                     self.radius,
                                                                     self.sparse)
-        sigma_sorround_weights_matrix = get_gaussian_weights_variable(self.input_shape, self.self_shape,
-                                                                      self.sigma_sorround,
+        sigma_surround_weights_matrix = get_gaussian_weights_variable(self.input_shape, self.self_shape,
+                                                                      self.sigma_surround,
                                                                       self.radius,
                                                                       self.sparse)
         if self.on:
-            diff = (sigma_center_weights_matrix - sigma_sorround_weights_matrix).t()
+            diff = (sigma_center_weights_matrix - sigma_surround_weights_matrix).t()
         else:
-            diff = (sigma_sorround_weights_matrix - sigma_center_weights_matrix).t()
+            diff = (sigma_surround_weights_matrix - sigma_center_weights_matrix).t()
         self.register_buffer(name='afferent_weights', tensor=diff)
         self.weights = [('afferent_weights', self.afferent_weights)]
-
-    def forward(self, lgn_input):
-        self.input = lgn_input
-        matmul = self.matmul(self.input, self.afferent_weights)
-        self.activation = torch.autograd.Variable(self.piecewise_sigmoid(self.min_theta, self.max_theta,
-                                                                         self.lgn_factor * matmul))
-        return self.activation
-
-    def matmul(self, vector, matrix):
-        if not self.sparse:
-            return torch.matmul(vector.data, matrix)
-        else:
-            # Pytorch implements sparse matmul only sparse x dense -> sparse and sparse x dense -> dense,
-            # That's why it's reversed
-            return torch.matmul(matrix.t(), vector.data.t()).t()
