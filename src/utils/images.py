99f3984f0af9010bcfe0efb5039793f6e14d4e32
hernanbari <hernanfb@live.com.ar>
2017-11-01 22:58:59 -0300

    Adds vrange params to plot_matrix

--- src/utils/images.py
+++ src/utils/images.py
@@ -116,8 +116,8 @@ def debug():
     Pdb().set_trace()
 
 
-def plot_matrix(img):
-    plt.imshow(img, cmap='gray', vmin=0, vmax=1)
+def plot_matrix(img, vmin=0, vmax=1):
+    plt.imshow(img, cmap='gray', vmin=vmin, vmax=vmax)
     plt.show()
 
 
