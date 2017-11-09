11c8bd9c3642582846468d8c10ad55ee0dd551c3
hernanbari <hernanfb@live.com.ar>
2017-11-09 09:20:18 -0300

    Fixes CortexHebbian

--- src/supervised_gcal/optimizers.py
+++ src/supervised_gcal/optimizers.py
@@ -1,12 +1,12 @@
 import torch
-from src.supervised_gcal.layers.cortex_layer import CortexLayer
+from src.supervised_gcal.layers.modules.lissom import register_recursive_input_output_hook, Cortex
 from src.supervised_gcal.utils.functions import kill_neurons, linear_decay
 from src.supervised_gcal.utils.weights import apply_circular_mask_to_weights
 
 
 class CortexOptimizer(torch.optim.Optimizer):
     def __init__(self, cortex_layer):
-        assert isinstance(cortex_layer, CortexLayer)
+        assert isinstance(cortex_layer, Cortex)
         self.cortex_layer = cortex_layer
         super(CortexOptimizer, self).__init__(cortex_layer.parameters(), {})
 
@@ -29,9 +29,10 @@ class CortexHebbian(CortexOptimizer):
     def __init__(self, cortex_layer, learning_rate):
         super(CortexHebbian, self).__init__(cortex_layer)
         self.learning_rate = learning_rate
+        self.handles = register_recursive_input_output_hook(cortex_layer)
 
     def step(self, **kwargs):
-        self.hebbian_learning(self.cortex_layer.weights, self.cortex_layer.input, self.cortex_layer.activation,
+        self.hebbian_learning(self.cortex_layer.weight, self.cortex_layer.input, self.cortex_layer.output,
                               self.learning_rate, self.cortex_layer.radius)
 
     @staticmethod
@@ -39,12 +40,10 @@ class CortexHebbian(CortexOptimizer):
         # Weight adaptation of a single neuron
         # w'_pq,ij = (w_pq,ij + alpha * input_pq * output_ij) / sum_uv (w_uv,ij + alpha * input_uv * output_ij)
 
-        delta = learning_rate * torch.matmul(torch.t(input.data), output.data)
-        weights.data.add_(delta)
-        apply_circular_mask_to_weights(weights.data.t_(), radius)
-        weights.data.t_()
-        den = torch.norm(weights.data, p=1, dim=0)
-        weights.data.div_(den)
+        delta = learning_rate * torch.matmul(input.data.t(), output.data)
+        apply_circular_mask_to_weights(delta.t_(), radius)
+        weights.data.add_(delta.t_())
+        weights.data = torch.nn.functional.normalize(weights.data, p=1, dim=0)
         return
 
 
