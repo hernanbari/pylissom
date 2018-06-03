eb23e0d70deed5234409c4500d4e001d8ddf4b2b
hernanbari <hernanfb@live.com.ar>
2018-06-03 13:04:04 -0300

    Removes tf src and tutorials

--- tf_src/src/supervised_gcal/utils.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-
-def get_zeros(shape):
-    return tf.zeros(shape=shape, dtype=tf.float32, name='zeros')
-
-
-def get_ones(shape):
-    return tf.ones(shape=shape, dtype=tf.float32, name='ones')
