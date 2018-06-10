f7d32a1cec5fb56290c1b36acde14984dab161d8
hernanbari <hernanfb@live.com.ar>
2018-06-10 17:40:07 -0300

    Comments html static path

--- docs/conf.py
+++ docs/conf.py
@@ -94,12 +94,12 @@ html_theme_options = {
     'logo_only': True,
 }
 
-html_favicon = 'pylissom_logo.png'
+html_favicon = '_static/pylissom_logo.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+# html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
