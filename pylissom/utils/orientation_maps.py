3172bc544d46390bf29be802ac06339d00891efc
hernanbari <hernanfb@live.com.ar>
2018-06-18 20:52:29 -0300

    Adds docstring to init (test docs)

--- pylissom/utils/orientation_maps.py
+++ pylissom/utils/orientation_maps.py
@@ -16,6 +16,7 @@ from pylissom.utils.training.pipeline import Pipeline
 
 
 class OrientationMap(object):
+    r"""FOOOOOOO"""
     # TODO: optimize using vectorization to calculate activations
     def __init__(self, model, inputs, use_tqdm_notebook=True):
         """
