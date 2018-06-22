77132935ed550bc5b52e8601aa803c8ad3c1cdda
hernanbari <hernanfb@live.com.ar>
2018-06-21 22:51:58 -0300

    Mocks pytorch in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,18 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sys
+# noinspection PyCompatibility
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['torch']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 
