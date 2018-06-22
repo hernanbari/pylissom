e3d84b9c2d331d9b91814658901879438db9bbc3
hernanbari <hernanfb@live.com.ar>
2018-06-22 00:38:40 -0300

    Fixes docstrings

--- pylissom/optim/optimizers.py
+++ pylissom/optim/optimizers.py
@@ -14,7 +14,7 @@ from pylissom.nn.modules.lissom import Cortex
 torch.optim.Optimizer.__module__ = 'torch.optim'
 
 
-class CortexOptimizer(torch.optim.Optimizer):
+class CortexOptimizer(object):
     r"""Abstract py:class:`torch.optim.Optimizer` that can only be used with py:class:`pylissom.nn.modules.Cortex`"""
     def __init__(self, cortex):
         assert isinstance(cortex, Cortex)
