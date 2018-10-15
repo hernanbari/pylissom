b8b9a5b3e9aefb1e6f2ba84d21116a951e089a77
hernanbari <hernanfb@live.com.ar>
2018-10-15 20:51:17 -0300

    Adds some ignores to raw sync

--- sync_files/raw_sync.sh
+++ sync_files/raw_sync.sh
@@ -6,10 +6,10 @@ else
     date
 
 
-    find . -not -path "./venv*" -not -path "./resources/caches*" -not -path "./torch*" -not -path "./pytorch" | grep py$ | grep -v npy > sync_files/files
-    find . -not -path "./venv*" -not -path "./resources/caches*" -not -path "./torch*" -not -path "./pytorch" | grep yaml$ >> sync_files/files
-    find . -not -path "./venv*" -not -path "./resources/caches*" -not -path "./torch*" -not -path "./pytorch" | grep sql$ >> sync_files/files
-    find . -not -path "./venv*" -not -path "./resources/caches*" -not -path "./torch*" -not -path "./pytorch" | grep ini$ >> sync_files/files
+    find . -not -path "./venv*" -not -path "./resources/caches*" -not -path "./torch*" -not -path "./pytorch*" -not -path "./vision-master*" -not -path "./build*" | grep py$ | grep -v npy > sync_files/files
+    find . -not -path "./venv*" -not -path "./resources/caches*" -not -path "./torch*" -not -path "./pytorch*" -not -path "./vision-master*" -not -path "./build*" | grep yaml$ >> sync_files/files
+    find . -not -path "./venv*" -not -path "./resources/caches*" -not -path "./torch*" -not -path "./pytorch*" -not -path "./vision-master*" -not -path "./build*" | grep sql$ >> sync_files/files
+    find . -not -path "./venv*" -not -path "./resources/caches*" -not -path "./torch*" -not -path "./pytorch*" -not -path "./vision-master*" -not -path "./build*" | grep ini$ >> sync_files/files
 
     rsync -r --partial --files-from=sync_files/files --rsh=ssh . hbari@cerebro.dc.uba.ar:$cerebro_path
     echo Ended
