19f680967a14e1fe31f5dc5226080533c1806b43
hernanbari <hernanfb@live.com.ar>
2018-10-16 08:13:49 -0300

    Changes test_math to pytest syntax

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,25 +1,27 @@
-import unittest
+import pytest
 
 from pylissom.math import *
 
 
-class TestMath(unittest.TestCase):
+class TestMath(object):
     """
     Test class for the math module
     """
 
-    def test_euclidean(self):
-        self.assertEqual(euclidian_distances(1, 1, 1, 1), 0)
-        self.assertEqual(euclidian_distances(1, 0, 0, 0), 1)
-        self.assertEqual(euclidian_distances(1, 2, 1, -2), 4)
-        self.assertEqual(euclidian_distances(0, 0, 1, 2), np.sqrt(5))
+    @pytest.mark.parametrize("x, y, mu_x, mu_y, expected", [
+        (1, 1, 1, 1, 0),
+        (1, 0, 0, 0, 1),
+        (1, 2, 1, -2, 4),
+        (0, 0, 1, 2, np.sqrt(5)),
+    ])
+    def test_euclidean(self, x, y, mu_x, mu_y, expected):
+        assert euclidian_distances(x, y, mu_x, mu_y) == expected
 
-    def test_euclidean_general(self):
-        self.assertEqual(euclidean_distance_general(np.array([1, 1]), np.array([1, 1])), 0)
-        self.assertEqual(euclidean_distance_general(np.array([1, 0]), np.array([0, 0])), 1)
-        self.assertEqual(euclidean_distance_general(np.array([1, 2]), np.array([1, -2])), 4)
-        self.assertEqual(euclidean_distance_general(np.array([0, 0]), np.array([1, 2])), np.sqrt(5))
-
-
-suite = unittest.TestLoader().loadTestsFromTestCase(TestMath)
-unittest.TextTestRunner(verbosity=2).run(suite)
+    @pytest.mark.parametrize("x_vector, y_vector, expected", [
+        (np.array([1, 1]), np.array([1, 1]), 0),
+        (np.array([1, 0]), np.array([0, 0]), 1),
+        (np.array([1, 2]), np.array([1, -2]), 4),
+        (np.array([0, 0]), np.array([1, 2]), np.sqrt(5)),
+    ])
+    def test_euclidean_general(self, x_vector, y_vector, expected):
+        assert euclidean_distance_general(x_vector, y_vector) == expected
