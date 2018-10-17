632ef6baf2081aaac0bb747901405b2c43ce4883
hernanbari <hernanfb@live.com.ar>
2018-10-16 22:48:54 -0300

    Adds hebbian test

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 import numpy as np
 
+from pylissom.nn.functional.weights import apply_circular_mask_to_weights
 from pylissom.nn.modules import Cortex
 from pylissom.optim import SequentialOptimizer, CortexHebbian, ReducedLissomHebbian, ConnectionDeath, NeighborsDecay
 from pylissom.utils.stimuli import gaussian_generator
@@ -79,15 +80,34 @@ class TestOptimizers(object):
         pass
 
     def test_cortex_hebbian(self, cortex, learning_rate, gaussian_variable):
-        previous_weights = cortex.weight.data.numpy()
-        inp = gaussian_variable.data.numpy()
+        previous_weights = cortex.weight.data.numpy().copy()
+        inp = gaussian_variable.data.numpy().copy()
+        assert inp.T.shape[1] == 1
         cortex_hebbian = CortexHebbian(cortex, learning_rate)
         out = cortex(gaussian_variable)
-        out = out.data.numpy()
+        out = out.data.numpy().copy()
         assert np.allclose(previous_weights, cortex.weight.data.numpy())
         assert np.allclose(inp, cortex.input.data.numpy())
         cortex_hebbian.step()
         assert np.allclose(inp, cortex.input.data.numpy())
+        hebbian_weights = self.hebbian_calculation(cortex, inp, learning_rate, out, previous_weights)
+        assert np.allclose(cortex.weight.data.numpy(), hebbian_weights)
+
+    def hebbian_calculation(self, cortex, inp, learning_rate, out, previous_weights):
+        # w'_pq,ij = (w_pq,ij + alpha * input_pq * output_ij) / sum_uv (w_uv,ij + alpha * input_uv * output_ij)
+        corr_matrix = np.matmul(inp.T, out)
+        corr_matrix = apply_circular_mask_to_weights(torch.from_numpy(corr_matrix), cortex.radius).numpy()
+        corr_matrix_norm = np.sum(corr_matrix, axis=0)
+        previous_weights_norm = np.sum(previous_weights, axis=0)
+        hebbian_weights = np.zeros(previous_weights.shape)
+        for idx, x in np.ndenumerate(cortex.weight.data.numpy()):
+            col_sum = previous_weights_norm[idx[1]]
+            den_term = (col_sum + learning_rate * corr_matrix_norm[idx[1]])
+            nom_term = (previous_weights[idx] + learning_rate * inp[0][idx[0]] * out[0][idx[1]])
+            new_weight = nom_term / den_term
+            hebbian_weights[idx] = new_weight
+        hebbian_weights = apply_circular_mask_to_weights(torch.from_numpy(hebbian_weights), cortex.radius).numpy()
+        return hebbian_weights
 
     def test_reduced_lissom_hebbian(self):
         pass
